c9f9f4e2e95adacdd86eff58f6b4fe4ab002bdd5
justynajanczy <justyna5252@gmail.com>
2021-11-22 00:11:40 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "LinkedList.h"
+#include <stdio.h>
+
+int main()
+{
+	linkedlist_t myList = linked_list_create();
+	linked_list_add_item(myList, 1);
+	linked_list_add_item(myList, 2);
+	linked_list_add_item(myList, 3);
+	//printf("Length: %d\n", linked_list_no_of_items("%d\n", myList));
+
+
+	linked_list_print_elements(myList);
+	printf("pull method result: %d\n", linked_list_pull_item(myList));
+	linked_list_print_elements(myList);
+
+	printf("%d\n", linked_list_peekItemByIndex(myList, 1));
+}
